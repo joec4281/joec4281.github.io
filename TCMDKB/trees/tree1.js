6ebdf1f1e4a19c35aa6b3da16e72f4648894b1d1
JoeC4281 <58880711+JoeC4281@users.noreply.github.com>
2021-10-13 12:05:35 -0400

    All

---
+++ TCMDKB/trees/tree1.js
@@ -0,0 +1,124 @@
+﻿var tree_data1 = [
+  { value: "Overview", id: "1", link: "1", open: true, data: [
+    { value: "TCC", id: "2", link: "2", open: true, data: [
+      { value: "Commands", id: "3", link: "3", open: true, data: [
+        { value: "Commands By Name", id: "16", link: "16" },
+        { value: "?", id: "17", link: "17" },
+        { value: "CLIPMONITOR", id: "55", link: "55" },
+        { value: "DISKMONITOR", id: "45", link: "45" },
+        { value: "ENDLOCAL", id: "66", link: "66" },
+        { value: "EVENTLOG", id: "77", link: "77", open: true, data: [
+          { value: "Source", id: "79", link: "79", open: true, data: [
+            { value: "Create", id: "81", link: "81" },
+            { value: "Delete", id: "80", link: "80" },
+            { value: "Exist", id: "85", link: "85" }          ]
+          },
+          { value: "Clear", id: "86", link: "86"          },
+          { value: "Exist", id: "84", link: "84"          },
+          { value: "Read Entry", id: "83", link: "83"          },
+          { value: "Registry", id: "78", link: "78"          },
+          { value: "Write Entry", id: "82", link: "82"          }        ]
+        },
+        { value: "FONT", id: "56", link: "56"        },
+        { value: "GZIP", id: "57", link: "57"        },
+        { value: "IDE", id: "46", link: "46"        },
+        { value: "LIBRARY", id: "27", link: "27", open: true, data: [
+          { value: "25.0.17", id: "28", link: "28" }        ]
+        },
+        { value: "LUA", id: "33", link: "33", data: [
+          { value: "26.01.36", id: "34", link: "34" }        ]
+        },
+        { value: "PROPS", id: "37", link: "37"        },
+        { value: "PSHELL", id: "5", link: "5", open: true, data: [
+          { value: "Date Math", id: "7", link: "7" },
+          { value: "Error launching COM", id: "14", link: "14" },
+          { value: "Get-PEHeader", id: "8", link: "8" },
+          { value: "GetCommandLine", id: "13", link: "13" },
+          { value: "Get-EventLog", id: "109", link: "109" },
+          { value: "GetObject", id: "101", link: "101" },
+          { value: "JScript", id: "105", link: "105" },
+          { value: "Multi WMI Query", id: "15", link: "15" },
+          { value: "PSHELL Notes", id: "6", link: "6" },
+          { value: "Read Nth Line Of A Test File", id: "9", link: "9" },
+          { value: "Split-String", id: "10", link: "10" }        ]
+        },
+        { value: "SCRIPT", id: "102", link: "102", open: true, data: [
+          { value: "DynamicWrapperX", id: "103", link: "103" }        ]
+        },
+        { value: "SENDHTML", id: "62", link: "62"        },
+        { value: "SETARRAY", id: "47", link: "47"        },
+        { value: "SETERROR", id: "48", link: "48"        },
+        { value: "SHELLEX", id: "35", link: "35", data: [
+          { value: "Creating a .WSH file", id: "36", link: "36" }        ]
+        },
+        { value: "STATUSBAR", id: "49", link: "49"        },
+        { value: "TAR", id: "58", link: "58"        },
+        { value: "TASKBAR", id: "50", link: "50"        },
+        { value: "TCC", id: "68", link: "68"        },
+        { value: "TCDIALOG", id: "63", link: "63"        },
+        { value: "TEE", id: "30", link: "30", open: true, data: [
+          { value: "26.1.40.0", id: "32", link: "32" }        ]
+        },
+        { value: "UNSETARRAY", id: "51", link: "51"        },
+        { value: "UNGZIP", id: "52", link: "52"        },
+        { value: "UNQLITE", id: "112", link: "112"        },
+        { value: "UNTAR", id: "60", link: "60"        },
+        { value: "UNZIP", id: "59", link: "59"        },
+        { value: "VBEEP", id: "53", link: "53"        },
+        { value: "VIEW", id: "64", link: "64", open: true        },
+        { value: "WEBFORM", id: "67", link: "67"        },
+        { value: "ZIP", id: "54", link: "54"        },
+        { value: "ZIPSFX", id: "61", link: "61"        }      ]
+      },
+      { value: "PlugIns", id: "38", link: "38", data: [
+        { value: "SysUtils", id: "39", link: "39" }      ]
+      },
+      { value: "tcmdupdate.aiu", id: "18", link: "18", data: [
+        { value: "27.0.18", id: "65", link: "65" },
+        { value: "26.1.40.0", id: "29", link: "29" },
+        { value: "25.0.27.0", id: "26", link: "26" },
+        { value: "24.02.50.0", id: "25", link: "25" },
+        { value: "23.0.34.0", id: "22", link: "22" },
+        { value: "22.0.42.0", id: "21", link: "21" },
+        { value: "21.1.61.0", id: "24", link: "24" },
+        { value: "20.11.46.0", id: "19", link: "19" }      ]
+      },
+      { value: "Unicode", id: "87", link: "87", data: [
+        { value: "OPTION", id: "88", link: "88" }      ]
+      },
+      { value: "DiskPart", id: "89", link: "89", data: [
+        { value: "Detail Disk", id: "92", link: "92" },
+        { value: "List Disk", id: "90", link: "90" },
+        { value: "List vDisk", id: "97", link: "97" },
+        { value: "Select Disk", id: "91", link: "91" },
+        { value: "vhdDrive.txt", id: "96", link: "96" },
+        { value: "PowerShell", id: "99", link: "99", open: true, data: [
+          { value: "Get-DiskImage", id: "93", link: "93", open: true, data: [
+            { value: "Get-Disk", id: "94", link: "94", open: true, data: [
+              { value: "Get-Partition", id: "95", link: "95" }            ]
+            }          ]
+          },
+          { value: "Get-Disk", id: "98", link: "98"          }        ]
+        }      ]
+      },
+      { value: "EVENTCREATE", id: "106", link: "106"      },
+      { value: "wevtutil", id: "107", link: "107", data: [
+        { value: "qe", id: "108", link: "108" }      ]
+      },
+      { value: "Zone.Identifier", id: "104", link: "104"      },
+      { value: "Functions", id: "110", link: "110", open: true, data: [
+        { value: "@wmi", id: "111", link: "111" }      ]
+      }    ]
+    },
+    { value: "TCMD", id: "69", link: "69", data: [
+      { value: "18", id: "70", link: "70" },
+      { value: "20", id: "71", link: "71" },
+      { value: "22", id: "72", link: "72" }    ]
+    },
+    { value: "CMDebug", id: "73", link: "73", data: [
+      { value: "21", id: "75", link: "75" },
+      { value: "22", id: "74", link: "74" },
+      { value: "23", id: "76", link: "76" }    ]
+    }  ]
+  }
+];
