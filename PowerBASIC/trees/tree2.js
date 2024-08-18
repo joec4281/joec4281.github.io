5ab56aefb9ee021f6254026ac5981274e03d5e91
JoeC4281 <58880711+JoeC4281@users.noreply.github.com>
2024-08-18 09:28:33 -0400

    Added PowerBASIC WebBook
    As above

---
+++ PowerBASIC/trees/tree2.js
@@ -0,0 +1,98 @@
+﻿var tree_data2 = [
+  { value: "Default Note", id: "2", link: "2", open: true, data: [
+    { value: "PowerBASIC Console Compiler", id: "3", link: "3", data: [
+      { value: "Better to put numbers in equations or use a global variable_ - PowerBASIC Peer Support Community (2024-08-15 5", id: "56", link: "56" },
+      { value: "Compiling and linking - PowerBASIC Peer Support Community - Personal", id: "16", link: "16", open: true },
+      { value: "HotKeys for PBCC program? - PowerBASIC Peer Support Community - Personal", id: "37", link: "37" },
+      { value: "just FASTPROC arguments not FUNCTION - PowerBASIC Peer Support Community (2024-08-15 9", id: "45", link: "45" },
+      { value: "Managing CPU usage for math-intense operations", id: "39", link: "39" },
+      { value: "PBCC Menu Dll - PowerBASIC Peer Support Community - Personal", id: "9", link: "9" },
+      { value: "Request example of png image reading and storage in a two-dimensional array in memory - PowerBASIC Peer Support Community - Personal", id: "13", link: "13" },
+      { value: "THREAD thread.bas - PowerBASIC Peer Support Community - Personal", id: "31", link: "31" }    ]
+    },
+    { value: "PowerBASIC For Windows", id: "20", link: "20", data: [
+      { value: "Calling a PB DLL from IIS/ASP? - PowerBASIC Peer Support Community - Personal", id: "36", link: "36" },
+      { value: "Open Pipe CMD - PowerBASIC Peer Support Community - Personal", id: "12", link: "12" },
+      { value: "stdout from a PB 10 program - PowerBASIC Peer Support Community - Personal", id: "27", link: "27", open: true, data: [
+        { value: "stdout from a PB 10 program - PowerBASIC Peer Support Community - Personal", id: "28", link: "28" }      ]
+      },
+      { value: "Visual Designers - supported? - PowerBASIC Peer Support Community - Personal", id: "22", link: "22"      },
+      { value: "Do you need a 64-bit compiler_ - PowerBASIC Peer Support Community (2024-08-15 4", id: "52", link: "52", open: true, data: [
+        { value: "Do you need a 64-bit compiler_ - PowerBASIC Peer Support Community (2024-08-15 4", id: "53", link: "53" },
+        { value: "Do you need a 64-bit compiler_ - PowerBASIC Peer Support Community (2024-08-15 4", id: "54", link: "54" },
+        { value: "Do you need a 64-bit compiler_ - PowerBASIC Peer Support Community (2024-08-15 4", id: "55", link: "55" }      ]
+      }    ]
+    },
+    { value: "PowerBASIC Inline Assembler", id: "17", link: "17", data: [
+      { value: "Arithmetic Expression Compiler - PowerBASIC Peer Support Community - Personal", id: "10", link: "10" }    ]
+    },
+    { value: "Programming", id: "19", link: "19", data: [
+      { value: "64 Bit Compiler - PowerBASIC Peer Support Community - Personal", id: "24", link: "24", data: [
+        { value: "language agnostic - Learning to write a compiler - Stack Overflow - Personal", id: "25", link: "25" },
+        { value: "Writing 64-bit programs - Personal", id: "26", link: "26" }      ]
+      },
+      { value: "Cmd.exe? - PowerBASIC Peer Support Community - Personal", id: "23", link: "23"      },
+      { value: "How to recognize executables compiled with powerbasic - _ _ PowerBASIC Peer Support Community (2024-08-15 4", id: "51", link: "51"      },
+      { value: "Input in Graphics Screen - PBCC 6.0 - PowerBASIC Peer Support Community - Personal", id: "15", link: "15"      },
+      { value: "Rosetta Code - PowerBASIC Peer Support Community - Personal", id: "32", link: "32"      }    ]
+    },
+    { value: "Programming With Objects", id: "21", link: "21", data: [
+      { value: "This VB6 COM DLL works from PBCC, but a few questions... - PowerBASIC Peer Support Community - Personal", id: "11", link: "11" }    ]
+    },
+    { value: "Source Code", id: "18", link: "18", data: [
+      { value: "Console output for either compiler - PowerBASIC Peer Support Community - Personal", id: "14", link: "14" },
+      { value: "Hebrew Date - PowerBASIC Peer Support Community - Personal", id: "29", link: "29" },
+      { value: "Microsoft Script Control wrapper functions - PowerBASIC Peer Support Community - Personal", id: "30", link: "30" },
+      { value: "How to load a string into RAM and allow a second program to access it - PowerBASIC Peer Support Community and 2 more pages - Personal", id: "38", link: "38" },
+      { value: "In-File Dynamic String Store", id: "40", link: "40", open: true }    ]
+    },
+    { value: "Jose's Corner", id: "33", link: "33", data: [
+      { value: "Source Code", id: "34", link: "34", open: true, data: [
+        { value: "Assorted COM examples - PowerBASIC Peer Support Community - Personal", id: "35", link: "35" }      ]
+      }    ]
+    },
+    { value: "Friends Of PowerBASIC", id: "41", link: "41", data: [
+      { value: "Third-Party Forums", id: "42", link: "42", open: true, data: [
+        { value: "MyLittleGrid - PowerBASIC Peer Support Community (2024-08-15 9", id: "43", link: "43" },
+        { value: "JK-IDE Version 2.0 including Visual Designer V 1.0 - PowerBASIC Peer Support Community (2024-08-15 9", id: "44", link: "44" }      ]
+      }    ]
+    },
+    { value: "Source Code Library", id: "47", link: "47", data: [
+      { value: "Console output for either compiler - PowerBASIC Peer Support Community (2024-08-16 7", id: "62", link: "62" },
+      { value: "Create ASMDATA block from a file - Actual Source Code. - PowerBASIC Peer Support Community (2024-08-16 7", id: "65", link: "65" },
+      { value: "Evaluate math expression - PowerBASIC Peer Support Community (2024-08-16 7", id: "72", link: "72" },
+      { value: "Extract Functions to create new programs - PowerBASIC Peer Support Community (2024-08-16 7", id: "69", link: "69" },
+      { value: "Finding a Window by its (partial) Tit", id: "71", link: "71" },
+      { value: "Get Command Line Arguments - PowerBASIC Peer Support Community (2024-08-16 7", id: "63", link: "63" },
+      { value: "Get$ and Redim At for reading binary files - PowerBASIC Peer Support Community (2024-08-16 7", id: "75", link: "75" },
+      { value: "Hide process from taskmanager - PowerBASIC Peer Support Community (2024-08-16 7", id: "70", link: "70" },
+      { value: "How to build SLL and library file in a new help format - PowerBASIC Peer Support Community (2024-08-16 7", id: "64", link: "64" },
+      { value: "I have just added a new tool for converting any file to an SLL. - PowerBASIC Peer Support Community (2024-08-16 7", id: "66", link: "66" },
+      { value: "I have just posted a 101 function library for PowerBASIC - PowerBASIC Peer Support Community (2024-08-16 7", id: "67", link: "67" },
+      { value: "In-File Dynamic String Store - PowerBASIC Peer Support Community (2024-08-15 1", id: "48", link: "48" },
+      { value: "Memory Mapped Files - PowerBASIC Peer Support Community (2024-08-16 7", id: "74", link: "74" },
+      { value: "Sharing Data between Processes - PowerBASIC Peer Support Community (2024-08-16 7", id: "73", link: "73" },
+      { value: "Tool for making ASMDATA procedures in PowerBASIC - PowerBASIC Peer Support Community (2024-08-16 7", id: "68", link: "68" },
+      { value: "Very fast character counter - PowerBASIC Peer Support Community (2024-08-16 6", id: "61", link: "61" }    ]
+    },
+    { value: "Special Interest Groups", id: "57", link: "57", open: true, data: [
+      { value: "VB Conversion", id: "58", link: "58", open: true, data: [
+        { value: "FreeDDT -need help locating - PowerBASIC Peer Support Community (2024-08-15 1", id: "50", link: "50" }      ]
+      },
+      { value: "COM Programming", id: "59", link: "59", open: true, data: [
+        { value: "NEWCOM CLSID with vbRichClient RC6 - PowerBASIC Peer Support Community (2024-08-15 6", id: "60", link: "60" },
+        { value: "COM event does not work. What have I missed_ - PowerBASIC Peer Support Community (2024-08-18 8", id: "76", link: "76" },
+        { value: "VBScript using a PB COM DLL - PowerBASIC Peer Support Community (2024-08-18 9", id: "77", link: "77" }      ]
+      },
+      { value: "Working With Objects", id: "78", link: "78", open: true, data: [
+        { value: "Need complete, simple example of a PB_Win COM DLL with calling VBSCRIPT code - PowerBASIC Peer Support Community (2024-08-18 9", id: "79", link: "79", open: true, data: [
+          { value: "Need complete, simple example of a PB_Win COM DLL with calling VBSCRIPT code - PowerBASIC Peer Support Community (2024-08-18 9", id: "80", link: "80", open: true, data: [
+            { value: "Need complete, simple example of a PB_Win COM DLL with calling VBSCRIPT code - PowerBASIC Peer Support Community (2024-08-18 9", id: "81", link: "81", open: true, data: [
+              { value: "Need complete, simple example of a PB_Win COM DLL with calling VBSCRIPT code - PowerBASIC Peer Support Community (2024-08-18 9", id: "82", link: "82" }            ]
+            }          ]
+          }        ]
+        }      ]
+      }    ]
+    }  ]
+  }
+];
