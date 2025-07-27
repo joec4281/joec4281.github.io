b2194f937901e1aa60cb67386740c32c04420e6d
JoeC4281 <58880711+JoeC4281@users.noreply.github.com>
2025-07-27 09:17:13 -0400

    Update
    Added VerInfoEx forum post.

---
+++ TCCPlugins/trees/tree2.js
@@ -0,0 +1,248 @@
+﻿var tree_data2 = [
+  { value: "Overview", id: "2", link: "2", open: true, data: [
+    { value: "TCC", id: "3", link: "3", open: true, data: [
+      { value: "PlugIns", id: "4", link: "4", open: true, data: [
+        { value: "4Console", id: "5", link: "5", data: [
+          { value: "Commands", id: "196", link: "196" },
+          { value: "Functions", id: "197", link: "197", open: true, data: [
+            { value: "@CHARAT", id: "199", link: "199" },
+            { value: "@REVCO", id: "200", link: "200" }          ]
+          },
+          { value: "Variables", id: "198", link: "198", open: true, data: [
+            { value: "_FONTINFO", id: "201", link: "201" },
+            { value: "_FONTINFO2", id: "202", link: "202" },
+            { value: "_HWNDCON", id: "203", link: "203" },
+            { value: "_RULER", id: "204", link: "204" }          ]
+          }        ]
+        },
+        { value: "4Utils", id: "6", link: "6", data: [
+          { value: "Commands", id: "30", link: "30", data: [
+            { value: "DEC", id: "105", link: "105" },
+            { value: "DROPTOCLIP", id: "106", link: "106" },
+            { value: "EVAL", id: "107", link: "107" },
+            { value: "GSET", id: "108", link: "108" },
+            { value: "INC", id: "109", link: "109" },
+            { value: "LC", id: "110", link: "110" },
+            { value: "NOOP", id: "111", link: "111" },
+            { value: "TODROP", id: "112", link: "112" },
+            { value: "UHELP", id: "113", link: "113" },
+            { value: "UPDATEENV", id: "114", link: "114" }          ]
+          },
+          { value: "Functions", id: "31", link: "31", open: true, data: [
+            { value: "@DOWCVT", id: "99", link: "99" },
+            { value: "@FILEREADI", id: "100", link: "100" },
+            { value: "@FILEREADX", id: "101", link: "101" },
+            { value: "@GEV", id: "102", link: "102" },
+            { value: "@GV", id: "103", link: "103" },
+            { value: "@NOOP", id: "104", link: "104" }          ]
+          },
+          { value: "Variables", id: "32", link: "32", open: true, data: [
+            { value: "_ENVVARS", id: "115", link: "115" },
+            { value: "_ENVCOUNT", id: "116", link: "116" },
+            { value: "_N", id: "117", link: "117" },
+            { value: "_TCCCOUNT", id: "118", link: "118" }          ]
+          }        ]
+        },
+        { value: "ActiveScript", id: "7", link: "7", open: true, data: [
+          { value: "Commands", id: "72", link: "72", open: true, data: [
+            { value: "AScript", id: "73", link: "73" },
+            { value: "DateDiffd", id: "74", link: "74" },
+            { value: "DateDiffm", id: "75", link: "75" },
+            { value: "DateDiffy", id: "76", link: "76" },
+            { value: "VB", id: "77", link: "77", open: true, data: [
+              { value: "VBScript One-Liners", id: "80", link: "80" }            ]
+            },
+            { value: "VBR", id: "78", link: "78"            },
+            { value: "VerInfoEx", id: "79", link: "79", open: true, data: [
+              { value: "EXE file detail fields | Take Command / TCC and 1 more page - Personal", id: "98", link: "98" }            ]
+            }          ]
+          }        ]
+        },
+        { value: "BBStuff", id: "8", link: "8", data: [
+          { value: "Commands", id: "185", link: "185", open: true, data: [
+            { value: "BBDUMP", id: "188", link: "188" },
+            { value: "BBLIST", id: "189", link: "189" }          ]
+          },
+          { value: "Functions", id: "186", link: "186", data: [
+            { value: "@BALLOC", id: "190", link: "190" },
+            { value: "@BFILL", id: "191", link: "191" },
+            { value: "@BFREE", id: "192", link: "192" },
+            { value: "@BFREEALL", id: "193", link: "193" },
+            { value: "@BLOAD", id: "194", link: "194" },
+            { value: "BSAVE", id: "195", link: "195" }          ]
+          },
+          { value: "Variables", id: "187", link: "187"          }        ]
+        },
+        { value: "CMath", id: "9", link: "9"        },
+        { value: "dbf", id: "10", link: "10", data: [
+          { value: "Commands", id: "33", link: "33" },
+          { value: "Functions", id: "34", link: "34", open: true, data: [
+            { value: "@BytesInHeader", id: "119", link: "119" },
+            { value: "@dbfVersion", id: "120", link: "120" },
+            { value: "@RecCount", id: "121", link: "121" },
+            { value: "@FieldCount", id: "122", link: "122" },
+            { value: "@FirstRecordOffset", id: "123", link: "123" },
+            { value: "@LUpdate", id: "124", link: "124" },
+            { value: "@RecNum", id: "125", link: "125" },
+            { value: "@Record", id: "126", link: "126" },
+            { value: "@RecSize", id: "127", link: "127" }          ]
+          },
+          { value: "Variables", id: "35", link: "35"          },
+          { value: "Scripts", id: "36", link: "36", open: true, data: [
+            { value: "TestDBF.btm", id: "128", link: "128" },
+            { value: "ThisMonth.btm", id: "129", link: "129" }          ]
+          }        ]
+        },
+        { value: "EditKeys", id: "11", link: "11"        },
+        { value: "EKeys", id: "12", link: "12"        },
+        { value: "Elevated", id: "13", link: "13"        },
+        { value: "FileUtils", id: "14", link: "14", data: [
+          { value: "Commands", id: "81", link: "81", open: true, data: [
+            { value: "APPPATHS", id: "84", link: "84" },
+            { value: "BOTTOMUP", id: "85", link: "85" },
+            { value: "BSPLIT", id: "86", link: "86" },
+            { value: "DIRCMP", id: "87", link: "87" },
+            { value: "EMPTIES", id: "88", link: "88" },
+            { value: "FILEINFO", id: "89", link: "89" },
+            { value: "FILEUTILSHELP", id: "90", link: "90" },
+            { value: "RENCASE", id: "91", link: "91" },
+            { value: "SWAPNAMES", id: "92", link: "92" }          ]
+          },
+          { value: "Functions", id: "82", link: "82", data: [
+            { value: "@IMGINFO", id: "93", link: "93" },
+            { value: "@WINPROP", id: "94", link: "94" }          ]
+          },
+          { value: "Variables", id: "83", link: "83", data: [
+            { value: "_FWDRIVES", id: "95", link: "95" },
+            { value: "_K32VER", id: "96", link: "96" },
+            { value: "_USBDRIVES", id: "97", link: "97" }          ]
+          }        ]
+        },
+        { value: "Hashmap", id: "15", link: "15", data: [
+          { value: "Commands", id: "145", link: "145" },
+          { value: "Functions", id: "146", link: "146", open: true, data: [
+            { value: "@hashnew", id: "144", link: "144" }          ]
+          },
+          { value: "Variables", id: "147", link: "147"          },
+          { value: "BasicTest.btm", id: "148", link: "148"          }        ]
+        },
+        { value: "Ruler", id: "16", link: "16"        },
+        { value: "SafeChars", id: "17", link: "17", data: [
+          { value: "Commands", id: "176", link: "176", open: true, data: [
+            { value: "UNSAFE", id: "184", link: "184" }          ]
+          },
+          { value: "Functions", id: "177", link: "177", open: true, data: [
+            { value: "@CountSafe", id: "178", link: "178" },
+            { value: "@SafeArray", id: "179", link: "179" },
+            { value: "@SafeEcho", id: "180", link: "180" },
+            { value: "@SafeEnv", id: "181", link: "181", open: true, data: [
+              { value: "Remap Unicode to ASCII", id: "183", link: "183" }            ]
+            },
+            { value: "@CountUnsafe", id: "182", link: "182"            }          ]
+          }        ]
+        },
+        { value: "Sift", id: "18", link: "18"        },
+        { value: "SMBStuff", id: "19", link: "19"        },
+        { value: "SysUtils", id: "20", link: "20", data: [
+          { value: "Commands", id: "25", link: "25", open: true, data: [
+            { value: "EXPORT", id: "130", link: "130" },
+            { value: "PROPS", id: "131", link: "131" },
+            { value: "VOLUME", id: "132", link: "132" },
+            { value: "WHICHWIN", id: "133", link: "133" },
+            { value: "PRNTRS", id: "134", link: "134" },
+            { value: "PSTAT", id: "135", link: "135" },
+            { value: "SHELLEX", id: "136", link: "136" },
+            { value: "MOVEWIN", id: "137", link: "137" }          ]
+          },
+          { value: "Functions", id: "26", link: "26", open: true, data: [
+            { value: "@EXETYPE", id: "140", link: "140" },
+            { value: "@FGWIN", id: "141", link: "141" },
+            { value: "@PSET", id: "142", link: "142" },
+            { value: "@WHICHWIN", id: "143", link: "143" }          ]
+          }        ]
+        },
+        { value: "TextUtils", id: "21", link: "21", data: [
+          { value: "Commands", id: "27", link: "27", open: true, data: [
+            { value: "CLIP2TEXT", id: "37", link: "37" },
+            { value: "CONTEXT", id: "38", link: "38" },
+            { value: "COPYCHARS", id: "39", link: "39" },
+            { value: "COUNTCHARS", id: "40", link: "40" },
+            { value: "DEDUP", id: "41", link: "41" },
+            { value: "DEGAS", id: "42", link: "42" },
+            { value: "DEHTML", id: "43", link: "43" },
+            { value: "FFIELDS", id: "44", link: "44" },
+            { value: "FILTERFILES", id: "45", link: "45" },
+            { value: "LOADARRAY", id: "46", link: "46" },
+            { value: "OINK", id: "47", link: "47" },
+            { value: "PARSEARGS", id: "48", link: "48" },
+            { value: "PASSWORD", id: "49", link: "49" },
+            { value: "RECASE", id: "50", link: "50" },
+            { value: "REPLACETEXT", id: "51", link: "51" },
+            { value: "ROT13", id: "52", link: "52" },
+            { value: "SAVEARRAY", id: "53", link: "53" },
+            { value: "SHUFFLE", id: "54", link: "54" },
+            { value: "TEXT2CLIP", id: "55", link: "55" },
+            { value: "TEXTUTILSHELP", id: "56", link: "56" },
+            { value: "UNICODIFY", id: "57", link: "57" },
+            { value: "UPEND", id: "58", link: "58" },
+            { value: "UTYPE", id: "59", link: "59" },
+            { value: "WORDS", id: "60", link: "60" },
+            { value: "WRAP", id: "61", link: "61" },
+            { value: "XFILTER", id: "62", link: "62" }          ]
+          },
+          { value: "Functions", id: "28", link: "28", open: true, data: [
+            { value: "@B85TOBIN", id: "63", link: "63" },
+            { value: "@BETWEEN", id: "64", link: "64" },
+            { value: "@BINTOB85", id: "65", link: "65" },
+            { value: "@CLARIFY", id: "66", link: "66" },
+            { value: "@INIVALUE", id: "67", link: "67" },
+            { value: "@METAPHONE", id: "68", link: "68" },
+            { value: "@MKENTITIES", id: "69", link: "69" },
+            { value: "@OBSCURE", id: "70", link: "70" },
+            { value: "@OINK", id: "71", link: "71" }          ]
+          },
+          { value: "Variables", id: "29", link: "29", open: true, data: [
+            { value: "_getacp", id: "138", link: "138", open: true, data: [
+              { value: "CHCP Difference", id: "139", link: "139" }            ]
+            }          ]
+          }        ]
+        },
+        { value: "UIStuff", id: "22", link: "22"        },
+        { value: "WindowInfo", id: "23", link: "23", data: [
+          { value: "Commands", id: "149", link: "149" },
+          { value: "Functions", id: "150", link: "150", open: true, data: [
+            { value: "@HWNDPID", id: "152", link: "152" },
+            { value: "@PIDHWND", id: "153", link: "153" },
+            { value: "@PIDHWNDS", id: "154", link: "154" },
+            { value: "@TITLEHWND", id: "155", link: "155" },
+            { value: "@WINALPHA", id: "156", link: "156" },
+            { value: "@WINMONITOR", id: "157", link: "157" },
+            { value: "@WINOWNER", id: "158", link: "158" },
+            { value: "@WINPARENT", id: "159", link: "159" },
+            { value: "@WINSNAPPED", id: "160", link: "160" },
+            { value: "@WINSTYLES", id: "161", link: "161" },
+            { value: "@WINSTYLESEX ", id: "162", link: "162" },
+            { value: "@WINVDESK", id: "163", link: "163" }          ]
+          },
+          { value: "Variables", id: "151", link: "151"          }        ]
+        },
+        { value: "ISO8601", id: "24", link: "24", data: [
+          { value: "Commands", id: "164", link: "164", open: true, data: [
+            { value: "QCAL", id: "167", link: "167" }          ]
+          },
+          { value: "Functions", id: "165", link: "165", open: true, data: [
+            { value: "@Date", id: "168", link: "168" },
+            { value: "@DatePlus", id: "169", link: "169" },
+            { value: "@Days", id: "170", link: "170" },
+            { value: "@DowIs", id: "171", link: "171" },
+            { value: "@InRange", id: "172", link: "172" },
+            { value: "@Julian", id: "173", link: "173" },
+            { value: "@MakeDate", id: "174", link: "174" },
+            { value: "@Ord", id: "175", link: "175" }          ]
+          },
+          { value: "Variables", id: "166", link: "166", open: true          }        ]
+        }      ]
+      }    ]
+    }  ]
+  }
+];
