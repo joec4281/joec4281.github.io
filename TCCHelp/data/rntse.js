129947cd8cb9acb3ef8cc2735ed5cee3e6f1a6b0
JoeC4281 <58880711+JoeC4281@users.noreply.github.com>
2026-08-17 07:03:19 -0400

    Add TCCHelp section

---
+++ TCCHelp/data/rntse.js
@@ -0,0 +1,49 @@
+﻿var SearchEngine = (function () {
+    function SearchEngine() {
+        this.results = new ResultList();
+        this.hl_str = "";
+    }
+    SearchEngine.prototype.isMatch = function (word, term) {
+        if (word.indexOf(term) != -1)
+            return true;
+        else
+            return false;
+    };
+    SearchEngine.prototype.search = function (s) {
+        this.results.clear();
+        var input = s.split(/ +/);
+        this.hl_str = "?highlight=1";
+        var _results = new Array();
+        for (var i = 0; i < input.length; i++) {
+            _results.push(new ResultList());
+            this.hl_str += "," + input[i];
+        }
+        for (var i = 0; i < input.length; i++)
+            for (var j = 0; j < searchArr.length; j += 2) {
+                if (this.isMatch(searchArr[j], input[i])) {
+                    for (var k = 0; k < searchArr[j + 1].length; k++) {
+                        _results[i].add(searchArr[j + 1][k][0], searchArr[j + 1][k][1]);
+                    }
+                }
+            }
+        for (var i = 0; i < _results.length; i++) {
+        }
+        for (var i = 0; i < _results[0].count(); i++) {
+            var occ = 1;
+            var score = _results[0].getScore(i);
+            var id = _results[0].getId(i);
+            for (var j = 1; j < input.length; j++) {
+                var idx = _results[j].indexOfId(id);
+                if (idx > -1) {
+                    occ += 1;
+                    score += _results[j].getScore(idx);
+                }
+            }
+            if (occ === input.length)
+                this.results.add(id, score);
+        }
+        this.results.sort2Desc();
+        //this.results.trace();
+    };
+    return SearchEngine;
+})();
---
+++ TCCHelp/trees/tree2.js
@@ -0,0 +1,163 @@
+﻿var tree_data2 = [
+  { value: "Default Note", id: "2", link: "2", open: true, data: [
+    { value: "What's New in...", id: "3", link: "3", open: true, data: [
+      { value: "Whats New in Version 33", id: "5", link: "5" },
+      { value: "Whats New in Version 32", id: "6", link: "6" },
+      { value: "Whats New in Version 31", id: "7", link: "7" }    ]
+    },
+    { value: "TCC", id: "4", link: "4", open: true, data: [
+      { value: "Variables & Functions", id: "8", link: "8", data: [
+        { value: "Variables", id: "10", link: "10", data: [
+          { value: "_BATCHLABEL (New in v32)", id: "12", link: "12", open: true },
+          { value: "_DARKENABLED", id: "13", link: "13" },
+          { value: "_DARKSUPPORTED", id: "14", link: "14" },
+          { value: "_HIGHCONTRAST", id: "15", link: "15" },
+          { value: "_PBATCHNAME (New In v31)", id: "16", link: "16" },
+          { value: "_VOLUME", id: "17", link: "17", data: [
+            { value: "New In v31", id: "107", link: "107" }          ]
+          }        ]
+        },
+        { value: "Functions", id: "11", link: "11", open: true, data: [
+          { value: "@ASCIIX", id: "18", link: "18" },
+          { value: "@BUSTYPE", id: "19", link: "19" },
+          { value: "@CHAR", id: "20", link: "20" },
+          { value: "@LUA", id: "21", link: "21" },
+          { value: "@MEDIATYPE", id: "22", link: "22" },
+          { value: "@PID", id: "23", link: "23" },
+          { value: "@TCFONT", id: "24", link: "24" },
+          { value: "@TMP (New In v31)", id: "25", link: "25" },
+          { value: "@TMPWN (New In v31)", id: "26", link: "26" },
+          { value: "@UNICODE", id: "27", link: "27" },
+          { value: "@UNICODEX", id: "28", link: "28" },
+          { value: "@UNIQUEX", id: "29", link: "29" },
+          { value: "@XHISTORY (New in v32)", id: "30", link: "30" }        ]
+        }      ]
+      },
+      { value: "Commands", id: "9", link: "9", open: true, data: [
+        { value: "ACTIVATE", id: "31", link: "31", data: [
+          { value: "New In v33", id: "70", link: "70" }        ]
+        },
+        { value: "ALIAS", id: "32", link: "32", data: [
+          { value: "New In v33", id: "71", link: "71" }        ]
+        },
+        { value: "CALLER (New In v32)", id: "33", link: "33"        },
+        { value: "CD / CHDIR", id: "34", link: "34", data: [
+          { value: "New In v33", id: "72", link: "72" }        ]
+        },
+        { value: "COPY", id: "35", link: "35", data: [
+          { value: "New In v33", id: "73", link: "73" },
+          { value: "New In v32", id: "74", link: "74" }        ]
+        },
+        { value: "DIRENV (New In v30)", id: "36", link: "36", open: true, data: [
+          { value: ".envtc and the TEMP environment variable", id: "110", link: "110" }        ]
+        },
+        { value: "DIRHISTORY", id: "37", link: "37", data: [
+          { value: "New In v32", id: "106", link: "106" }        ]
+        },
+        { value: "DO", id: "38", link: "38", data: [
+          { value: "New In v33", id: "75", link: "75" },
+          { value: "New In v32", id: "76", link: "76" }        ]
+        },
+        { value: "ENDLOCAL", id: "39", link: "39", data: [
+          { value: "New In v33", id: "77", link: "77" }        ]
+        },
+        { value: "EXEC (New In v32)", id: "40", link: "40"        },
+        { value: "FOR", id: "41", link: "41", data: [
+          { value: "New In v33", id: "78", link: "78" }        ]
+        },
+        { value: "FALSE (New In v32)", id: "42", link: "42"        },
+        { value: "FOLDERMONITOR - Monitor Windows folders and files", id: "43", link: "43", open: true, data: [
+          { value: "Example", id: "109", link: "109" }        ]
+        },
+        { value: "FSEARCH (New In v32)", id: "44", link: "44", data: [
+          { value: "New In v33", id: "79", link: "79" }        ]
+        },
+        { value: "IF", id: "45", link: "45", data: [
+          { value: "New In v33", id: "80", link: "80" }        ]
+        },
+        { value: "INTERNAL (New In v32)", id: "46", link: "46", data: [
+          { value: "New In v33", id: "81", link: "81" }        ]
+        },
+        { value: "LUA", id: "47", link: "47", data: [
+          { value: "New In v33", id: "82", link: "82" }        ]
+        },
+        { value: "MAPPEXE (New In v34)", id: "118", link: "118"        },
+        { value: "MOVE", id: "48", link: "48", data: [
+          { value: "New In v33", id: "83", link: "83" },
+          { value: "New In v32", id: "84", link: "84" }        ]
+        },
+        { value: "MSGBOX", id: "49", link: "49", data: [
+          { value: "New In v33", id: "85", link: "85" }        ]
+        },
+        { value: "OPTION", id: "50", link: "50", data: [
+          { value: "New In v32", id: "105", link: "105" }        ]
+        },
+        { value: "POPD", id: "51", link: "51", data: [
+          { value: "New In v32", id: "102", link: "102" }        ]
+        },
+        { value: "PUSHD", id: "52", link: "52", data: [
+          { value: "New In v32", id: "103", link: "103" }        ]
+        },
+        { value: "RANDOM (New In v32)", id: "53", link: "53"        },
+        { value: "RD / RMDIR", id: "54", link: "54", data: [
+          { value: "New In v32", id: "104", link: "104" }        ]
+        },
+        { value: "SETLOCAL", id: "55", link: "55", data: [
+          { value: "New In v33", id: "86", link: "86" }        ]
+        },
+        { value: "SHORTCUT", id: "56", link: "56", data: [
+          { value: "New In v33", id: "87", link: "87" }        ]
+        },
+        { value: "SREPLACE", id: "57", link: "57", data: [
+          { value: "New In v33", id: "88", link: "88" }        ]
+        },
+        { value: "TEE", id: "58", link: "58", data: [
+          { value: "New In v33", id: "89", link: "89" }        ]
+        },
+        { value: "TASKDIALOG", id: "59", link: "59", data: [
+          { value: "New In v33", id: "90", link: "90" }        ]
+        },
+        { value: "TOUCH", id: "60", link: "60", data: [
+          { value: "New In v33", id: "91", link: "91" }        ]
+        },
+        { value: "TPIPE", id: "61", link: "61", data: [
+          { value: "New In v33", id: "92", link: "92" }        ]
+        },
+        { value: "TRUE (New In v32)", id: "62", link: "62"        },
+        { value: "WATCH (New In v32)", id: "63", link: "63", open: true, data: [
+          { value: "New In v33", id: "93", link: "93" },
+          { value: "New In v32", id: "94", link: "94" }        ]
+        },
+        { value: "WHICH", id: "64", link: "64", data: [
+          { value: "New In v33", id: "95", link: "95" }        ]
+        },
+        { value: "WINDOW", id: "65", link: "65", data: [
+          { value: "New In v33", id: "96", link: "96" }        ]
+        },
+        { value: "WSHELL", id: "66", link: "66", data: [
+          { value: "New In v33", id: "97", link: "97" }        ]
+        },
+        { value: "WSHORTCUT", id: "67", link: "67", data: [
+          { value: "New In v33", id: "98", link: "98" }        ]
+        },
+        { value: "XHISTORY (New In v32)", id: "68", link: "68", open: true, data: [
+          { value: "New In v33", id: "99", link: "99" },
+          { value: "New In v32", id: "100", link: "100" }        ]
+        },
+        { value: "XSORT (New In v33)", id: "69", link: "69", data: [
+          { value: "New In v33", id: "101", link: "101" }        ]
+        }      ]
+      },
+      { value: "Input / Output and Redirection", id: "115", link: "115", open: true, data: [
+        { value: "Redirection and Pipes", id: "116", link: "116", open: true, data: [
+          { value: "Redirection", id: "117", link: "117" }        ]
+        }      ]
+      }    ]
+    },
+    { value: "Reference", id: "111", link: "111", open: true, data: [
+      { value: "Plugins", id: "112", link: "112", open: true, data: [
+        { value: ".NET Plugins", id: "114", link: "114" }      ]
+      }    ]
+    }  ]
+  }
+];
